46459f1f6b58063e78b30e22041f31ced84610e4
Luis Villalobos <luisevm24@outlook.com>
2020-12-20 21:23:22 -0600

    lietaral template

---
+++ typescript/template-literal.ts
@@ -0,0 +1,16 @@
+(function(){
+
+    const name = 'Luis';
+    const lastname = 'Villalobos';
+    const age = 25;
+
+    //const output = name + " " + lastname + " ( " + age + " ) ";
+    //const output = `${name} ${lastname} ( ${age} )`;
+    const output = `
+    ${name}
+    ${lastname}
+    ( ${age + 1000} )
+    `;
+    console.log(output);
+
+})();
