608481c431805449abab11f7bb5fe3a7b4acde40
Luis Villalobos <luisevm24@outlook.com>
2021-01-18 17:55:55 -0600

    simple example of usage

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
-let button  = document.querySelector('button');
-button.addEventListener('click', (event) => { console.log(event) })
+
+const buttton = document.querySelector('button');
+
+
+Rx.Observable.fromEvent(buttton, 'click')
+.throttleTime(1000) // wait time to handle other click
+.map( (data) => { return [data.clientX, data.clientY]}) // handle the information passed
+.subscribe( (xy) => console.log(xy[0] + ' - ' + xy[1]) );
