0e76841fd4ed96c696d7788d7af1eac61fd1cc2c
Luis Villalobos <luisevm24@outlook.com>
2021-01-18 20:40:21 -0600

    create from scratch subscription, observer and observables

--- app.js
+++ app.js
@@ -1,8 +1,56 @@
 
 const buttton = document.querySelector('button');
 
+const observer = {
+    next: function(value){
+        console.log(value);
+    },
+    error: function(error) {
+        console.error(error);
+    },
+    complete: function() {
+        console.log('completed');
+    }
+};
 
+/*
+// Never end relation of observer
 Rx.Observable.fromEvent(buttton, 'click')
-.throttleTime(1000) // wait time to handle other click
-.map( (data) => { return [data.clientX, data.clientY]}) // handle the information passed
-.subscribe( (xy) => console.log(xy[0] + ' - ' + xy[1]) );
+.subscribe(observer);
+*/
+
+/*
+// Simple sync observer
+Rx.Observable.create(function(observe) {
+    observe.next('A value');
+    // observe.error('Finish obsevable');
+    setTimeout(function() {
+        observe.complete();
+    }, 2000);
+
+    observe.next('Try value');
+})
+.subscribe(observer);
+*/
+
+/*
+// Recreate onClick event
+Rx.Observable.create(function(observe) {
+    buttton.onclick = function(event) {
+        observe.next('Clicked in [' + event.clientX + '][' +event.clientY + ']');
+    }
+})
+.subscribe(observer);
+*/
+
+// TimeOut suscription
+const subscription = Rx.Observable.create(function(observe) {
+    buttton.onclick = function(event) {
+        observe.next('Clicked in [' + event.clientX + '][' +event.clientY + ']');
+    }
+})
+.subscribe(observer);
+
+setTimeout(function() {
+    subscription.unsubscribe();
+}, 5000);
